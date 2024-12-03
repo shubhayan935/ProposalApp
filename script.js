2c011cbe919a63b05b363955ba3a665d9ba861af
Shubhayan Srivastava <88150274+shubhayan935@users.noreply.github.com>
2024-12-03 04:29:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay!!!";
+  question.innerHTML = "I love you so much Tanisha!!!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
